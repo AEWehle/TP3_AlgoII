61715a5d8cfdcd6e3580c72e6303f20833ce667b
lnimo28 <115752347+lnimo28@users.noreply.github.com>
2022-11-23 20:40:23 -0300

    Create Algoritmo_camino_min.h

---
+++ Algoritmo_camino_min.h
@@ -0,0 +1,22 @@
+#ifndef ALGORITMO_CAMINO_MIN_H
+#define ALGORITMO_CAMINO_MIN_H
+#include "Lista.h"
+#include "Vertice.h"
+#include <iostream>
+
+class AlgoritmoCaminoMinimo{
+    // Atributos
+    protected:
+        int ** matriz_de_adyacencia;
+        Lista<Vertice>* lista_vertices;
+
+    // Métodos
+    public:
+        AlgoritmoCaminoMinimo(Lista<Vertice>* lista_vertices, int** matriz_de_adyacencia);
+
+        virtual void mostrar_camino_minimo(int origen, int destino) = 0;
+
+        virtual ~AlgoritmoCaminoMinimo() = default;
+};
+
+#endif // ALGORITMO_CAMINO_MIN_H
