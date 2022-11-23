89b714ab711a56c80e620be1a1c1279485e9eaef
lnimo28 <115752347+lnimo28@users.noreply.github.com>
2022-11-23 20:41:27 -0300

    Create Vertice.h

---
+++ Vertice.h
@@ -0,0 +1,22 @@
+#ifndef VERTICE_H
+#define VERTICE_H
+
+using namespace std;
+
+class Vertice {
+    // Atributos
+    private:
+        int vertice;
+
+    // Métodos
+    public:
+        Vertice(int vertice);
+
+        //post: obtiene el nombre del vertice
+        int obtener_nombre();
+
+        ~Vertice();
+};
+
+
+#endif //VERTICE_H
